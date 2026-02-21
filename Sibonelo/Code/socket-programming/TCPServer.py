668647dd848ce6f3d23fc303e96cac56d6c5e4bb
Kamohelo Mosikili <kamohelomosix145@gmail.com>
2026-02-21 21:31:27 +0200

    Updating Sibonelo Folder

--- Sibonelo/Code/socket-programming/TCPServer.py
+++ Sibonelo/Code/socket-programming/TCPServer.py
@@ -1,6 +1,6 @@
 from socket import *
 
-serverPort = 12000
+serverPort = 33060
 
 serverSocket = socket(AF_INET,SOCK_STREAM)
 
@@ -13,11 +13,19 @@ print("KeoTaba ready to mingle")
 while True:
 	connectionSocket, addr = serverSocket.accept()
 
+	print("Connection established...")
+
 	sentence = connectionSocket.recv(1024).decode()
+
+	print("Message Decoded")
 
 	moddedSentence = sentence.upper()
 
+	print("Sending message: ", moddedSentence)
+
 	connectionSocket.send( moddedSentence.encode() )
+
+	print("Message sent: Perhaps a Yay! is in order")
 
 	connectionSocket.close()
 
